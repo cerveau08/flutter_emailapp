d51aeb225cf83b6c73eb94192a4817283d7fe0fd
cerveau08 <malickcoly342@gmail.com>
2020-04-27 23:55:44 +0000

    Accessing Assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:emailapp/MessageList.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(EmailApp());
@@ -11,45 +12,10 @@ class EmailApp extends StatelessWidget {
         primarySwatch: Colors.blueGrey,
         accentColor: Colors.blue
       ),  
-      home: MyHomePage(title: 'Flutter EmailApp'),
+      home: MessageList(title: 'Flutter EmailApp'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  
-  final String title;
-  var messages = const [
-    'My first message',
-    'My second message',
-    'You won lottery',
-    'You should read',
-    'You won lottery again'
-  ];
-  MyHomePage({this.title});
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(this.title),
-        ),
-        body: ListView.separated(
-          itemCount: messages.length,
-          separatorBuilder: (context, index) => Divider(),
-          itemBuilder: (BuildContext context, int index) {
-            var title = messages[index];
-            return ListTile(
-              title: Text(title),
-              isThreeLine: true,
-              leading: CircleAvatar(
-                child: Text('Cerv'),
-              ),
-              subtitle: Text('Another text which is very very long so everyone can read it see it'),
-            );
-          },
-          ),
-        );
-  }
-  
-}
+
 
