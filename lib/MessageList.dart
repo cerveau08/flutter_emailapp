f240b1a068d0fcf2f0af8d941a890bc0ec2d412c
cerveau08 <malickcoly342@gmail.com>
2020-04-28 00:35:02 +0000

    Types & JSON Serialization

--- lib/MessageList.dart
+++ lib/MessageList.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:emailapp/Message.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -12,10 +13,12 @@ class MessageList extends StatefulWidget {
 class _MessageListState extends State<MessageList> {
   var messages = const [];
   Future loadMessageList() async {
-    var content = await rootBundle.loadString('data/message.json');
-    var collection = json.decode(content);
-    setState(() {
-      messages = collection; 
+    String content = await rootBundle.loadString('data/message.json');
+    List collection = json.decode(content);
+    List<Message> _messages = 
+      collection.map((json) => Message.fromJson(json)).toList();
+    setState(() { 
+      messages = _messages; 
     });
   }
   
@@ -33,15 +36,15 @@ class _MessageListState extends State<MessageList> {
           itemCount: messages.length,
           separatorBuilder: (context, index) => Divider(),
           itemBuilder: (BuildContext context, int index) {
-            var message = messages[index];
+            Message message = messages[index];
             return ListTile(
-              title: Text(message['subject']),
+              title: Text(message.subject),
               isThreeLine: true,
               leading: CircleAvatar(
                 child: Text('Cerv'),
               ),
               subtitle: Text(
-                message['body'],
+                message.body,
                 maxLines: 2,
                 overflow: TextOverflow.ellipsis,
                 ),
